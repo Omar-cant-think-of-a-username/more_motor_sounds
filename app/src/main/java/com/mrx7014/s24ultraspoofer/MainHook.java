f0692b2d1fd4f8b6c11a788a906659b30b4ac1c4
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-02-07 20:13:31 +0300

    Update MainHook.java

--- app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
+++ app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
@@ -26,85 +26,20 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {        
-        XposedBridge.log("s24ultraspoofer: Hooking into: " + lpparam.packageName);
+        XposedBridge.log("Colored_FP: Hooking into: " + lpparam.packageName);
+         if (!lpparam.packageName.equals("com.android.systemui")) {
+                 return;}
+         
+            // Target class and method
+             String targetClass = "com.coloros.systemui.keyguard.onscreenfingerprint.OnScreenFingerprintOpticalAnimCtrl";
+             String targetMethod = "updateFpIconColor";
 
-        // ro.product.manufacturer
-        XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "Samsung");
-
-        // ro.product.brand
-        XposedHelpers.setStaticObjectField(Build.class, "BRAND", "Samsung");
-
-        // ro.product.device
-        XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "e3q");
-
-        // ro.product.system.device
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.DEVICE", "e3q");
-
-        // ro.build.tags
-        XposedHelpers.setStaticObjectField(Build.class, "TAGS", "release-keys");
-
-        // ro.build.type
-        XposedHelpers.setStaticObjectField(Build.class, "TYPE", "user");
-
-        // ro.product.product
-        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", "e3q");
-
-        // ro.product.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.vendor.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system_ext.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system.name
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.NAME", "e3q");
-
-        // ro.product.vendor.name
-        XposedHelpers.setStaticObjectField(Build.class, "VENDOR.NAME", "e3q");
-
-        // ro.product.system_ext.name
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM_EXT.NAME", "e3q");
-
-        // ro.system_ext.model
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM_EXT.MODEL", "e3a");
-
-        // ro.product.model
-        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT.MODEL", "e3q");
-
-        // ro.product.vendor.model
-        XposedHelpers.setStaticObjectField(Build.class, "VENDOR.MODEL", "e3a");
-
-        // ro.product.system.model
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.MODEL", "e3a");
-
-        // ro.product.flavor
-        XposedHelpers.setStaticObjectField(Build.class, "FLAVOUR", "e3q-user");
-
-        // ro.soc.model
-        XposedHelpers.setStaticObjectField(Build.class, "SOC.MODEL", "SM8650-AC");
-        
-        // ro.gpu.model
-        XposedHelpers.setStaticObjectField(Build.class, "GPU.MODEL", "Adreno750");
-        
-        // ro.hardware.chipname
-        XposedHelpers.setStaticObjectField(Build.class, "HARDWARE.CHIPNAME", "Pineapple");
-        
-        // ro.build.version.oneui
-        XposedHelpers.setStaticObjectField(Build.class, "BUILD.VERSION,ONEUI", "60101");
-
-        // ro.product.board
-        XposedHelpers.setStaticObjectField(Build.class, "BOARD", "e3q");
-
-        // ro.build.id
-        XposedHelpers.setStaticObjectField(Build.class, "ID", "UP1A.231005.007");
-
-        // ro.build.fingerprint
-        XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT",
-                "samsung/e3q/e3q:14/UP1A.231005.007/S928BXXS3AXI7:user/release-keys");
+             // Hook the method
+             XposedHelpers.findAndHookMethod(targetClass, lpparam.classLoader, targetMethod, new XC_MethodHook() {
+                 @Override
+                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                     // Modify behavior before the original method is called
+                     Log.d("CameraSoundHook", "updateFpIconColor() hooked and bypassed!");
+       
     }
 }
