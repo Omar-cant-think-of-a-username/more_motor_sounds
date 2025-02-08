d85eed93dc780c94c8dd89fc34f0f37e35a714dd
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-02-08 16:36:17 +0300

    Update MainHook.java

--- app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
+++ app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
@@ -22,8 +22,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.callbacks.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook;
 
 public class MainHook implements IXposedHookLoadPackage {
 
